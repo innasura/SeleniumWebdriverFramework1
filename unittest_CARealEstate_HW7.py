971f7e9312de718b80e2dc9460157005ac8e0ac5
innasura <innasura12@gmail.com>
2020-03-26 19:48:15 -0700

    3/26/20

---
+++ unittest_CARealEstate_HW7.py
@@ -0,0 +1,236 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class ChromeSubmitForm(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    # As per unittest module, individual test should start with test_
+    def test_submit_form_chrome(self):
+        driver = self.driver
+        driver_chrome = self.driver
+        driver_chrome.get('https://qasvus.wordpress.com')
+        wait = WebDriverWait(driver, 3)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='pushbutton-wide']")))
+        time.sleep(1)  # simulate long running test
+
+        assert "California Real Estate" in driver.title
+        print("Page title in Chrome is:", driver.title)
+
+        # Clear fields: text area, first and last name
+        driver.find_element(By.ID, "g2-name").clear()
+        driver.find_element(By.ID, "g2-email").clear()
+        driver.find_element(By.NAME, "g2-message").clear()
+        time.sleep(1)
+
+        #Fill out all fields and click on Submit button
+        driver.find_element(By.ID, "g2-name").send_keys("INNA SURA")
+        driver.find_element(By.ID, "g2-email").send_keys("innasura12@gmail.com")
+        driver.find_element(By.NAME, "g2-message").send_keys("Python Homework #7 is done! (Chrome/Unittest)")
+        driver.implicitly_wait(10)
+        element = driver.find_element_by_class_name('pushbutton-wide')
+        driver.execute_script("arguments[0].click();", element)
+        driver.implicitly_wait(10)
+
+        # Use "try/except" method to wait "go back" link is VISIBLE and click it after
+        try:
+            WebDriverWait(driver, 10) \
+                .until(EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'go back')]")))
+            element = driver.find_element(By.XPATH, "//a[contains(text(),'go back')]")
+            driver.execute_script("arguments[0].click();", element)
+            print("The form is submitted!")
+        except TimeoutException:
+            print("Loading took too much time!")
+
+        # Use "wait.until" method for visibility of all 4 houses images on the main page
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-55']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-34']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-56']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-30']")))
+
+        # Do assertion for page title and print it with custom string
+        assert "California Real Estate" in driver.title
+        print("GO BACK Page title in Chrome is:", driver.title)
+
+    def test_submit_form_chrome_1120x550(self):
+        driver = self.driver
+        driver_chrome = self.driver
+        driver.set_window_size(1120, 550)
+        driver_chrome.get('https://qasvus.wordpress.com')
+        wait = WebDriverWait(driver, 3)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='pushbutton-wide']")))
+        time.sleep(1)
+
+        assert "California Real Estate" in driver.title
+        print("Page title in Chrome 1120x550 is:", driver.title)
+
+        # Clear fields: text area, first and last name
+        driver.find_element(By.ID, "g2-name").clear()
+        driver.find_element(By.ID, "g2-email").clear()
+        driver.find_element(By.NAME, "g2-message").clear()
+        time.sleep(1)
+
+        #Fill out all fields and click on Submit button
+        driver.find_element(By.ID, "g2-name").send_keys("INNA SURA")
+        driver.find_element(By.ID, "g2-email").send_keys("innasura12@gmail.com")
+        driver.find_element(By.NAME, "g2-message").send_keys("Python Homework #7 is done! (Chrome 1120x550/Unittest)")
+        driver.implicitly_wait(10)
+        element = driver.find_element_by_class_name('pushbutton-wide')
+        driver.execute_script("arguments[0].click();", element)
+        driver.implicitly_wait(10)
+
+        # Use "try/except" method to wait "go back" link is VISIBLE and click it after
+        try:
+            WebDriverWait(driver, 10) \
+                .until(EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'go back')]")))
+            element = driver.find_element(By.XPATH, "//a[contains(text(),'go back')]")
+            driver.execute_script("arguments[0].click();", element)
+            print("The form in Chrome 1120x550 is submitted!")
+        except TimeoutException:
+            print("Loading took too much time!")
+
+        # Use "wait.until" method for visibility of all 4 houses images on the main page
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-55']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-34']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-56']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-30']")))
+
+        # Do assertion for page title and print it with custom string
+        assert "California Real Estate" in driver.title
+        print("GO BACK Page title in Chrome 1120x550 is:", driver.title)
+
+    def tearDown(self):
+        # Close the browser.
+        self.driver.close()
+
+
+class FirefoxSubmitButton(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+
+    # As per unittest module, individual test should start with test_
+    def test_submit_button_firefox(self):
+        driver = self.driver
+        driver_ff = self.driver
+        driver_ff.get('https://qasvus.wordpress.com')
+        wait = WebDriverWait(driver, 3)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='pushbutton-wide']")))
+        time.sleep(1)  # simulate long running test
+
+        assert "California Real Estate" in driver.title
+        print("Page title in Firefox is:", driver.title)
+
+        # Clear fields: text area, first and last name
+        driver.find_element(By.ID, "g2-name").clear()
+        driver.find_element(By.ID, "g2-email").clear()
+        driver.find_element(By.NAME, "g2-message").clear()
+        time.sleep(1)
+
+        # Fill out all fields and click on Submit button
+        driver.find_element(By.ID, "g2-name").send_keys("INNA SURA")
+        driver.find_element(By.ID, "g2-email").send_keys("innasura12@gmail.com")
+        driver.find_element(By.NAME, "g2-message").send_keys("Python Homework #7 is done! (Firefox - Unittest)")
+        driver.implicitly_wait(10)
+        element = driver.find_element_by_class_name('pushbutton-wide')
+        driver.execute_script("arguments[0].click();", element)
+        driver.implicitly_wait(10)
+
+        # Use "try/except" method to wait "go back" link is VISIBLE and click it after
+        try:
+            WebDriverWait(driver, 10) \
+                .until(EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'go back')]")))
+            element = driver.find_element(By.XPATH, "//a[contains(text(),'go back')]")
+            driver.execute_script("arguments[0].click();", element)
+            print("The form in Firefox is submitted!")
+        except TimeoutException:
+            print("Loading took too much time!")
+
+        # Use "wait.until" method for visibility of all 4 houses images on the main page
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-55']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-34']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-56']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-30']")))
+
+        # Do assertion for page title and print it with custom string
+        assert "California Real Estate" in driver.title
+        print("GO BACK Page title in Firefox is:", driver.title)
+
+    def test_submit_button_firefox_1250x850(self):
+        driver = self.driver
+        driver_ff = self.driver
+        driver.set_window_size(1250, 850)
+        driver_ff.get('https://qasvus.wordpress.com')
+        wait = WebDriverWait(driver, 5)
+        wait.until(EC.visibility_of_element_located((By.XPATH, "//button[@class='pushbutton-wide']")))
+        time.sleep(1)  # simulate long running test
+
+        assert "California Real Estate" in driver.title
+        print("Page title in Firefox_1250x850 is:", driver.title)
+
+        # Clear fields: text area, first and last name
+        driver.find_element(By.ID, "g2-name").clear()
+        driver.find_element(By.ID, "g2-email").clear()
+        driver.find_element(By.NAME, "g2-message").clear()
+        time.sleep(1)
+
+        # Fill out all fields and click on Submit button
+        driver.find_element(By.ID, "g2-name").send_keys("INNA SURA")
+        driver.find_element(By.ID, "g2-email").send_keys("innasura12@gmail.com")
+        driver.find_element(By.NAME, "g2-message").send_keys("Python Homework #7 is done! (Firefox_1250x850 Unittest)")
+        driver.implicitly_wait(10)
+        element = driver.find_element_by_class_name('pushbutton-wide')
+        driver.execute_script("arguments[0].click();", element)
+        driver.implicitly_wait(10)
+
+        # Use "try/except" method to wait "go back" link is VISIBLE and click it after
+        try:
+            WebDriverWait(driver, 10) \
+                .until(EC.visibility_of_element_located((By.XPATH, "//a[contains(text(),'go back')]")))
+            element = driver.find_element(By.XPATH, "//a[contains(text(),'go back')]")
+            driver.execute_script("arguments[0].click();", element)
+            print("The form in Firefox_1250x850 is submitted!")
+        except TimeoutException:
+            print("Loading took too much time!")
+
+        # Use "wait.until" method for visibility of all 4 houses images on the main page
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-55']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-34']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-56']")))
+        WebDriverWait(driver, 10) \
+            .until(EC.visibility_of_element_located((By.XPATH, "//img[@class='wp-image-30']")))
+
+        # Do assertion for page title and print it with custom string
+        assert "California Real Estate" in driver.title
+        print("GO BACK Page title in Firefox_1250x850 is:", driver.title)
+
+
+    # Anything declared in tearDown will be executed for all test cases
+    def tearDown(self):
+        # Close the browser.
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
